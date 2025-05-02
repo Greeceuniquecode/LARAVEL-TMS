4bd93894ea2b0ca09b0e151836fb23b404c0c12b
Greeceuniquecode <notgreece2@gmail.com>
2025-05-02 17:49:30 +0545

    fixes

--- config/database.php
+++ config/database.php
@@ -42,10 +42,13 @@ return [
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'options' => [
+                PDO::ATTR_PERSISTENT => false,  // Disable persistent connections
+            ],
         ],
+
 
 
     ],
